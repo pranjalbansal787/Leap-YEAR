0228567e880b5de5f6997d6bccffa13c5d000828
Pranjal Bansal <pranban12@gmail.com>
2021-06-16 13:02:14 +0530

    Add files via upload

---
+++ leapyear.js
@@ -0,0 +1,15 @@
+// program to check leap year
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((year % 4==0) && ( year % 100=!0) || (year % 400==0)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+const year = prompt('Enter a year:');
+
+checkLeapYear(year)
